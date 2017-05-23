a39e0b535e9f2fee725504c26ceb9fe5eacad7e3
RoryLiao <RoryLiao@users.noreply.github.com>
2017-05-23 16:48:57 -0700

    Add files via upload

--- ForwardIndex.py
+++ ForwardIndex.py
@@ -5,13 +5,14 @@ import os
 import math
 from sys import argv
 
+
 class Word_Freq:
     def __init__(self):
         self.word_dict = {}
         self.doc_dict = {}
         self.doc_count = 0
         self.word_count = 0
-        
+
     # opens file and tokenizes it, adding it into a dictionary
     def open_f(self, f):
         try:
@@ -32,30 +33,30 @@ class Word_Freq:
         except IOError:
             print("This file does not exist: {0}".format(f))
 
-    #sorts the dict in descending order and prints it
+    # sorts the dict in descending order and prints it
     def print_word_dict(self, dir):
         for k, v in sorted(self.word_dict.items(), key=operator.itemgetter(1), reverse=True):
             print("{0} - {1}".format(k, v))
-            
+
     def clear_dict(self):
         self.word_dict.clear()
-    
+
     def iterate_directories(self, d):
-        #Open each file within a directory and make a forward index of the tokens
+        # Open each file within a directory and make a forward index of the tokens
         with open('Index.txt', 'a') as f:
             for subdir, dirs, files in os.walk(d):
                 for file in files:
                     if file == 'bookkeeping.json' or file == 'bookkeeping.tsv':
                         continue
                     self.open_f(os.path.join(subdir, file))
-                    
-                    #This nabs the beginning of the directory for URL lookup in the jsons
+
+                    # This nabs the beginning of the directory for URL lookup in the jsons
                     fileNumber = str(subdir)
                     while "\\" in fileNumber:
-                        fileNumber = fileNumber[fileNumber.rfind("\\")+1:]
-                    
-                    #This loop creates the inverted index
-                    for (k,tf) in self.word_dict.items():
+                        fileNumber = fileNumber[fileNumber.rfind("\\") + 1:]
+
+                    # This loop creates the inverted index
+                    for (k, tf) in self.word_dict.items():
                         if k not in self.doc_dict:
                             self.doc_dict[k] = [[fileNumber + "/" + str(file), tf]]
                         else:
@@ -63,31 +64,34 @@ class Word_Freq:
                     self.doc_count += 1
                     self.clear_dict()
 
-            #Exchange the tf out with the tf-idf of the token
-            for (k,v) in self.doc_dict.items():
+            # Exchange the tf out with the tf-idf of the token
+            for (k, v) in self.doc_dict.items():
                 tmpCount = 0
                 for e in v:
                     tf = e[1]
                     N = self.doc_count
                     df = len(self.doc_dict[k])
                     idf = math.log10(N / df)
-                    self.doc_dict[k][tmpCount].append(float(format(idf,'.2f')))
+                    self.doc_dict[k][tmpCount].append(float(format(idf, '.2f')))
                     tf_idf = math.log10(1 + int(tf)) * idf
                     self.doc_dict[k][tmpCount].append(float(format(tf_idf, '.2f')))
                     tmpCount += 1
 
-            #Printing out the inverted index to a text file for testing
-            #for (k,v) in self.doc_dict.items():
-                #print(str(k) + " - " + str(v) + "\n")
-                #f.write(str(k) + " - " + str(v) + "\n")
+                    # Printing out the inverted index to a text file for testing
+                    # for (k,v) in self.doc_dict.items():
+                    # print(str(k) + " - " + str(v) + "\n")
+                    # f.write(str(k) + " - " + str(v) + "\n")
 
-        #This is simply statistics output
-        self.word_count = len(self.doc_dict)
-        print("Document Count: " + str(self.doc_count))
-        print("Word Count: " + str(self.word_count))
+        # This is simply statistics output
+        # self.word_count = len(self.doc_dict)
+        # print("Document Count: " + str(self.doc_count))
+        # print("Word Count: " + str(self.word_count))
         return self.doc_dict
 
-
+    # def print_stats(self):
+    #     self.word_count = len(self.doc_dict)
+    #     print("Document Count: " + str(self.doc_count))
+    #     print("Word Count: " + str(self.word_count))
 def query_parse(argv):
     new_query = []
     if len(argv) == 1:
@@ -98,6 +102,7 @@ def query_parse(argv):
             new_query.append(x)
         return new_query
 
+
 def find_docs(token, index_dict):
     if token not in index_dict:
         return 'No results'
@@ -106,6 +111,7 @@ def find_docs(token, index_dict):
         for e in index_dict[token]:
             docs.append(e[0])
         return docs
+
 
 def make_url_dict(d):
     url_dict = {}
@@ -117,7 +123,7 @@ def make_url_dict(d):
                     if os.stat(f).st_size == 0:
                         print("This file is empty: {0}".format(f))
                         return
-                    for i in document: #for each line in the document
+                    for i in document:  # for each line in the document
                         '''
                         "0/0": "www.ics.uci.edu/~rickl/courses/cs-171/2016-smrq-cs171/StudentResources/StudentResources_tournament/Wumpus_World_tournament/Worlds_20160712/TestWorld5x5_787.txt",
                         '''
@@ -128,39 +134,41 @@ def make_url_dict(d):
                             url_dict[s1] = s2
     return url_dict
 
+
 def find_urls(url_dict, docIDs):
     results = []
     for e in docIDs:
         if e in url_dict:
             results.append(url_dict[e])
-            
+
     return results
-        
+
+
 if __name__ == "__main__":
     try:
-        #We store all data, such a tf, idf, tf-idf, type (e.g. title, bold, h1, h2, h3)
-        #We still need to append types
-        
-        #Accepts a query of any length
+        # We store all data, such a tf, idf, tf-idf, type (e.g. title, bold, h1, h2, h3)
+        # We still need to append types
+
+        # Accepts a query of any length
         query = query_parse(sys.argv)
         print(query[0])
 
-    	rootdir = 'WEBPAGES_CLEAN'
+        rootdir = 'WEBPAGES_CLEAN'
         w = Word_Freq()
         index = w.iterate_directories(rootdir)
-        
-        #We need to return all docIDs for the first word in the query M1 not M2
-        docIDs = find_docs(query[0], index)
+
+        # We need to return all docIDs for the first word in the query M1 not M2
+        docIDs = find_docs(str(query[0]).lower(), index)
         print(docIDs)
-        
-        #We need to use those docIDs to find the URLs and give them to the user
+
+        # We need to use those docIDs to find the URLs and give them to the user
         url_dict = make_url_dict(rootdir)
         results = find_urls(url_dict, docIDs)
         print("Search Results \n")
         for e in results:
             print(str(e) + "\n")
-        
-        print("Search Completed")
 
+        print("Search Completed")
+        #w.print_stats()
     except IndexError:
         print("No file was entered.")
